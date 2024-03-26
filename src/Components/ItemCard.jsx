78cb8594d1d8da8fdcc1a4373f81b424ca41820a
medoz3ro <bsabo44@gmail.com>
2024-03-26 15:57:36 +0100

    feat: Karijerko nav

---
+++ src/Components/ItemCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemCard() {
+  return (
+    <div>
+      Itemcard
+    </div>
+  )
+}
+
+export default ItemCard
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='border-solid'>
+      <h1 className='text-7xl text-green-300 border-solid'>Karijerko</h1>
+      <p></p>
+    </div>
+
+    
+  )
+}
+
+export default Navbar
