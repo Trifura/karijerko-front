db95dd572ab3591544e71f2dff4eb7c7eab165c1
Ivan Mikodanic <ivan.mikodanic@rimac-technology.com>
2024-05-21 11:04:38 +0200

    feat: bake in environment variables

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      VITE_API_BASE_URL: process.env.VITE_API_BASE_URL,
+      VITE_GOOGLE_OAUTH_CLIENT_ID: process.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
+    }
+  }
 })
