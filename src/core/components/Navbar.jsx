5ebcf99ad3388ea7724bd67af16e199d99fe066e
medoz3ro <bsabo44@gmail.com>
2024-06-07 00:49:30 +0200

    feat: mousedown
    Profile settings close if you press anywhere on the page

--- src/core/components/Navbar.jsx
+++ src/core/components/Navbar.jsx
@@ -6,25 +6,35 @@ import SignOut from "../../assets/icons/sign_out.svg";
 import Verified from "../../assets/icons/Verified.svg";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../../auth/store/actions.js";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Navbar({ showLink = true }) {
   const dispatch = useDispatch();
   const { isAuthenticated, account } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
-  const handleLogut = async () => {
+  const handleLogout = async () => {
     await dispatch(logout());
   };
 
-  const profileData = {
-    name: "Ime i Prezime",
-    mail: "benjo1ssssss23@gmail.com",
-  };
+  // Close the dropdown if clicked outside of it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const loginButton = (
     <Link
@@ -36,30 +46,27 @@ export default function Navbar({ showLink = true }) {
   );
 
   const accountButton = (
-    <div className="relative">
+    <div className="relative" ref={dropdownRef}>
       <div
         className="w-10 h-10 lg:w-11 lg:h-11 border-2 border-gray-400 rounded-full cursor-pointer"
         onClick={toggleDropdown}
       >
         <img
-          src={account.profilePicture}
+          src={account?.profilePicture}
           className="rounded-full w-full h-full"
           alt=""
         />
       </div>
       {dropdownOpen && (
-        <div className="absolute right-0 mt-2 w-32 py-2 bg-white border border-gray-200 rounded-md shadow-lg">
-          <div className="px-4 border-b-2 py-2 text-[10px]">
-            <div className="flex flex-row items-center">
-              <div className="truncate" title={profileData.name}>
-                {profileData.name}
+        <div className="absolute right-0 mt-2 w-[305px] py-2 bg-white border border-gray-200 rounded-md shadow-lg">
+          <div className="px-4 border-b-2 py-2">
+            <div className="text-[18px] flex flex-row items-center">
+              <div className="text-[18px] truncate">
+                {account.firstName} {account.lastName}
               </div>
-
-              <img className="w-3" src={Verified} alt="" />
+              <img className="w-4 ml-2" src={Verified} alt="" />
             </div>
-            <div className="truncate" title={profileData.mail}>
-              {profileData.mail}
-            </div>
+            <div className="text-Hare truncate">{account.email}</div>
           </div>
 
           <Link
@@ -67,16 +74,16 @@ export default function Navbar({ showLink = true }) {
             className="text-right block px-4 py-2 text-gray-800 hover:font-medium"
           >
             <div className="flex flex-row">
-              <img className="w-4 mr-1" src={Profile} alt="" />
-              <div>Moj Profil</div>
+              <img className="w-4 mr-2" src={Profile} alt="" />
+              <div className="">Moj Profil</div>
             </div>
           </Link>
           <div
-            onClick={handleLogut}
+            onClick={handleLogout}
             className="text-right block px-4 py-2 text-gray-800 cursor-pointer hover:font-medium"
           >
             <div className="flex flex-row">
-              <img className="w-4 mr-1" src={SignOut} alt="" />
+              <img className="w-4 mr-2" src={SignOut} alt="" />
               <div>Odjavi se</div>
             </div>
           </div>
@@ -86,7 +93,6 @@ export default function Navbar({ showLink = true }) {
   );
 
   const navbarButton = isAuthenticated ? accountButton : loginButton;
-
   return (
     <div className="flex px-6 lg:px-16 py-4 shadow-md fixed w-full top-0 z-40 bg-white justify-between">
       <Link to={"/"} className="w-fit flex flex-col">
