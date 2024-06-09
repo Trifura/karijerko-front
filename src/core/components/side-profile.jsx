052d27b8625d4164dca6e2ad48c53ce10d81cc7a
Ivan Mikodanic <ivanmikodanic9@gmail.com>
2024-06-10 00:44:08 +0200

    feat: side profile style

--- src/core/components/side-profile.jsx
+++ src/core/components/side-profile.jsx
@@ -3,6 +3,7 @@ import CircleCheck from "../../assets/icons/Check_circle.svg";
 import CircleUnCheck from "../../assets/icons/UnCheck_circle.svg";
 import Pin from "../../assets/icons/Pin_fill.svg";
 import "../../index.css";
+import {Link} from "react-router-dom";
 
 function SideProfile({ selectedProfileId, setSelectedProfileId, account, profiles }) {
 
@@ -13,33 +14,42 @@ function SideProfile({ selectedProfileId, setSelectedProfileId, account, profile
           <div className="flex">
             <div className="flex flex-row">
               <img
-                className="p-1 w-[50px] h-[50px] border-2 border-Swan rounded-full"
+                className="w-[50px] h-[50px] border-2 border-Swan rounded-full"
                 src={account.profilePicture}
                 alt=""
               />
             </div>
 
             <div className="ml-3">
-              <div className="font-medium flex flex-row underline underline-offset-4 items-center">
-                <div>{account.firstName} {account.lastName}</div>
+              <div className="font-bold flex flex-row text-lg items-center">
+                <Link to="/profile">{account.firstName} {account.lastName}</Link>
                 <div className="ml-1">
                   <img src={Verified} alt="" />
                 </div>
               </div>
-              <div className="w-[14px] h-[14px] flex flex-row items-center">
+              <div className="w-5 h-5 flex flex-row items-center">
                 <img src={Pin} alt="" />
-                <div className="text-md">Zagreb</div>
+                <div className="text-md font-medium">Zagreb</div>
               </div>
             </div>
           </div>
 
           <div className="mt-8">
-            <div>Profil</div>
-
+            <div className="text-lg font-medium">Profil</div>
+            {
+                profiles.length === 0 && (
+                        <Link to="/profile" className="mt-2 font-bold bg-Primary hover:bg-PrimaryDark text-white rounded-2xl pl-4 pr-8 py-0.5 flex items-center gap-3">
+                          <span className="text-white text-2xl mb-0.5">
+                            +
+                          </span>
+                          Napravi profil
+                        </Link>
+                )
+            }
             {profiles.map((profile) => (
-              <button
-                key={profile.id}
-                className="gap-2 flex flex-row items-center mt-2"
+                <button
+                    key={profile.id}
+                    className="gap-2 flex flex-row items-center mt-2"
                 onClick={() => setSelectedProfileId(profile.id)}
               >
                 <div className="flex items-center gap-2">
@@ -51,7 +61,7 @@ function SideProfile({ selectedProfileId, setSelectedProfileId, account, profile
                         )
                   }
 
-                  <div className="text-sm font-medium">{profile.name}</div>
+                  <div className="font-medium">{profile.name}</div>
                 </div>
               </button>
             ))}
