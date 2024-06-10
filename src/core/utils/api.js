33362510bd422e1ed7881243ee2ca9fb17c56071
Ivan Mikodanic <ivanmikodanic9@gmail.com>
2024-06-10 04:53:06 +0200

    fix: axios interceptor

--- src/core/utils/api.js
+++ src/core/utils/api.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import {getToken} from "../../auth/utils/TokenHelper.js";
+import axios from 'axios';
+import { getToken } from "../../auth/utils/TokenHelper.js";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
@@ -7,8 +7,20 @@ const api = axios.create({
     baseURL,
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': getToken() ? 'Bearer ' + getToken() : null
     }
-})
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = getToken();
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
